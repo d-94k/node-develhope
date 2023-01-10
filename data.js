2a2b57ffc65cee2993d8dc6036c79e3d5cdbe605
d-94k <86233674+d-94k@users.noreply.github.com>
2023-01-10 14:38:42 +0100

    completed exercise

--- data.js
+++ data.js
@@ -1,10 +1,13 @@
-## Log the data ##
+
 // Challenge 1:
 //
 // Use 2 different techniques to output the value of this variable with
 // a label, so we can easily identify it in the script output.
 
 const surprisingFact = "The bumblebee bat is the world's smallest mammal";
+
+console.log ("A surprising fact:", surprisingFact);
+console.log ("A surprising fact: %s", surprisingFact)
 
 // Challenge 2:
 //
@@ -32,16 +35,22 @@ const familyTree = [
   },
 ];
 
+console.log (JSON.stringify (familyTree, null, 2));
+console.dir (familyTree, {depth: null})
+
 // Challenge 3:
 //
 // Output a count value every time the importantTask function is called.
 // Reset the count value after 4 function calls.
 
-function importantTask() {}
+function importantTask() {
+    console.count();
+}
 
 importantTask();
 importantTask();
 importantTask();
 importantTask();
+console.countReset();
 importantTask();
 importantTask();
--- server.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import "express-async-errors";
-
-const app = express ();
-
-app.get ("/", (request, response) => response.send("Mwo!"));
-app.listen (3000, () => console.log ("Server is running"));
